43d2776eed07b054215ef99c8d7be3bcb2f76da1
Bryan Gomez <77309020+bryangomezcarranza@users.noreply.github.com>
2024-09-06 10:56:40 -0700

    created xcode project

---
+++ Zone Sharing/Zone_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zone_SharingApp.swift
+//  Zone Sharing
+//
+//  Created by Bryan Gomez on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Zone_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
